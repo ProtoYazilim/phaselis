a84de7e0aa396b6b032972c7bdc2ab7b809f76a4
Ali Berk Islam <aliberkislam@hotmail.com>
2024-10-30 23:00:16 +0300

    docs: Add Switch component documentation and implementation

--- src/components/switch/types.ts
+++ src/components/switch/types.ts
@@ -3,10 +3,8 @@ import { IFormPropTypes, SlotChildComponent } from "@phaselis/types";
 interface SwitchProps extends IFormPropTypes {
   value?: boolean;
   onChange?: (event: any, value: boolean) => void;
-  onPress?: () => void;
   duration?: number;
   style?: any;
-  size?: any;
   name?: string;
   showIcons?: boolean;
   LeftSlot?: SlotChildComponent;

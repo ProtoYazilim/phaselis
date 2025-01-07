4b5026c2b99764cfa07d5825720da89482290200
Ali Berk Islam <aliberkislam@hotmail.com>
2025-01-07 18:06:18 +0300

    Enhance Bottom Sheet story

--- app/stories/bottom-sheet/index.tsx
+++ app/stories/bottom-sheet/index.tsx
@@ -1,15 +1,19 @@
 import { Button, BottomSheet, Block } from "phaselis";
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 function BottomSheetStory() {
   const [isOpen1, setIsOpen1] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Block style={{ margin: 10, gap: 10 }}>
+      <Block style={{ margin: 24 }}>
         <Button
           text="Open Bottom Sheet"
+          variation="primary_outline"
+          leftIcon="Eye"
+          size="xl"
+          // style={{ container: { height: 96 } }}
           onClick={() => {
             setIsOpen1(true);
           }}
@@ -21,12 +25,15 @@ function BottomSheetStory() {
           setIsOpen1(false);
         }}
       >
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          dolores quos iure tenetur eius accusantium blanditiis, officia labore
-          molestias corporis enim consequatur cum deserunt quae error cumque,
-          eaque facilis dolorum!
-        </Text>
+        <View style={{ padding: 32 }}>
+          <Text>Suspendisse aliquam venenatis lacus feugiat condimentum.</Text>
+          <Text></Text>
+          <Text>
+            In in tristique risus, quis pretium nunc. Cras viverra augue ac urna
+            luctus feugiat. Pellentesque dapibus scelerisque elit, et semper
+            erat.
+          </Text>
+        </View>
       </BottomSheet>
     </SafeAreaView>
   );
